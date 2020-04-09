c06962a1e09d7ea049a562d46136b55acf39341a
Armagaiden <bourhisgwendal@gmail.com>
2020-04-09 16:20:44 +0200

    REDIRECTION INSCRIPTION

--- src/servlets/ServletInscription.java
+++ src/servlets/ServletInscription.java
@@ -35,7 +35,8 @@ public class ServletInscription extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/inscription.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
