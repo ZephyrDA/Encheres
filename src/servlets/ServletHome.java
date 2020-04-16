e06b6a913436eee08c901cde692003b79875988d
Armagaiden <bourhisgwendal@gmail.com>
2020-04-16 15:49:41 +0200

    Update ServletHome.java

--- src/servlets/ServletHome.java
+++ src/servlets/ServletHome.java
@@ -69,6 +69,11 @@ public class ServletHome extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set standard HTTP/1.1 no-cache headers.
+		response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+
+		// Set standard HTTP/1.0 no-cache header.
+		response.setHeader("Pragma", "no-cache");
 		int idCategorie = Integer.parseInt(request.getParameter("category"));
 		try {
 			EncheresManager encheresManager = new EncheresManager();
