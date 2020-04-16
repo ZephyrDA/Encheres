91d3653c42a0105f3e6c0a5e1e27dad4522bf0d9
ElianLevene <34088734+ElianLevene@users.noreply.github.com>
2020-04-16 09:33:28 +0200

    Connexion/deco

--- src/servlets/ServletDeconnexion.java
+++ src/servlets/ServletDeconnexion.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,12 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import be.BusinessException;
+import bll.EncheresManager;
+import bo.Article;
+import bo.Categorie;
+
 /**
  * Servlet implementation class ServletDeconnexion
  */
 @WebServlet("/Deconnexion")
 public class ServletDeconnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static EncheresManager manager = new EncheresManager();
+
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -32,6 +40,23 @@ public class ServletDeconnexion extends HttpServlet {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
 		session.removeAttribute("connectedUser");
+		ArrayList<Article> listArticles = new ArrayList<Article>();
+		try {
+			listArticles = manager.getLesArticles();
+		} catch (BusinessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ArrayList<Categorie> listCategories = new ArrayList<Categorie>();
+		try {
+			listCategories = manager.getLesCategories();
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("lesCategories", listCategories);
+		request.setAttribute("lesArticles", listArticles);
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
 		rd.forward(request, response);	}
 
